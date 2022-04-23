a3d2664d7e52e04ccece14d0d803b3bbfbe57349
Carlosjocaballero <56653674+Carlosjocaballero@users.noreply.github.com>
2022-04-23 12:27:02 -0400

    Added Valid Parentheisis

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isValid(string s){
+    stack<char> par;
+    for(int i = 0; i < s.length(); ++i ){
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+            par.push(s[i]);
+        else{
+            if( (par.empty() || (par.top() == '(' && s[i] != ')') || (par.top() == '{' && s[i] != '}') || (par.top() == '[' && s[i] != ']'))){
+                return false;
+            }
+            par.pop();
+        }
+    }
+    return par.empty();
+}
+
+int main (){
+    string s = "({})"
+    cout << isValid(s) << endl;
+}
